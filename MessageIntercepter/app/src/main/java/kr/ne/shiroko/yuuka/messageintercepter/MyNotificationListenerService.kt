ea03f9a681aaa29ded713110f38e6d36e5bf3a7d
if1live <libsora25@gmail.com>
2024-02-25 15:34:13 +0900

    notification 받아서 http로 보내기 대충 정리

--- MessageIntercepter/app/src/main/java/kr/ne/shiroko/yuuka/messageintercepter/MyNotificationListenerService.kt
+++ MessageIntercepter/app/src/main/java/kr/ne/shiroko/yuuka/messageintercepter/MyNotificationListenerService.kt
@@ -4,10 +4,18 @@ import android.app.Notification
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.json.JSONObject
 
 // https://devjaewoo.tistory.com/54
 class MyNotificationListenerService : NotificationListenerService() {
     private val TAG = "MyNotificationListenerService"
+
+    // TODO: 주소 수정
+    private var url = "http://192.168.0.103:3000/messages/"
+    private val sender = MessageSender(url)
 
     override fun onListenerConnected() {
         super.onListenerConnected()
@@ -25,12 +33,13 @@ class MyNotificationListenerService : NotificationListenerService() {
         val packageName: String = sbn?.packageName ?: "Null"
         val extras = sbn?.notification?.extras
 
-        val extraTitle: String = extras?.get(Notification.EXTRA_TITLE).toString()
-        val extraText: String = extras?.get(Notification.EXTRA_TEXT).toString()
-        val extraBigText: String = extras?.get(Notification.EXTRA_BIG_TEXT).toString()
-        val extraInfoText: String = extras?.get(Notification.EXTRA_INFO_TEXT).toString()
-        val extraSubText: String = extras?.get(Notification.EXTRA_SUB_TEXT).toString()
-        val extraSummaryText: String = extras?.get(Notification.EXTRA_SUMMARY_TEXT).toString()
+        // https://developer.android.com/reference/android/app/Notification 필요하면 뒤져서 더 뜯기
+        val extraTitle = extras?.getString(Notification.EXTRA_TITLE)
+        val extraText = extras?.getString(Notification.EXTRA_TEXT)
+        val extraBigText = extras?.getString(Notification.EXTRA_BIG_TEXT)
+        val extraInfoText = extras?.getString(Notification.EXTRA_INFO_TEXT)
+        val extraSubText = extras?.getString(Notification.EXTRA_SUB_TEXT)
+        val extraSummaryText = extras?.getString(Notification.EXTRA_SUMMARY_TEXT)
 
         Log.d(
             TAG, "onNotificationPosted:\n" +
@@ -42,5 +51,23 @@ class MyNotificationListenerService : NotificationListenerService() {
                 "SubText: $extraSubText\n" +
                 "SummaryText: $extraSummaryText\n"
         )
+
+        // TODO: 관심있는 패키지에서 발생한 항목만 전달해야한다.
+        val json = JSONObject()
+        json.put("packageName", packageName)
+        json.put("title", extraTitle)
+        json.put("text", extraText)
+        json.put("bigText", extraBigText)
+        json.put("infoText", extraInfoText)
+        json.put("subText", extraSubText)
+        json.put("summaryText", extraSummaryText)
+
+        // kotlin 비동기 어떻게 쓰는거지?
+        val myScope = CoroutineScope(Dispatchers.Default)
+        myScope.launch {
+            // 여기서 suspend 함수를 호출
+            sender.sendMessage(json)
+        }
+
     }
 }
--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -8,3 +8,9 @@ app.use("*", cors());
 app.get("/", async (c) => {
   return c.json({ ok: true });
 });
+
+app.post("/messages/", async (c) => {
+  const body = await c.req.json();
+  console.log("body", JSON.stringify(body, null, 2));
+  return c.json({ok: true});
+});
