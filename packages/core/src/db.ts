29a87a3309c7a0f8c6e343336010fe252b342c0a
if1live <libsora25@gmail.com>
2024-03-09 18:19:00 +0900

    데이터를 sqlite filedb에도 기록해서 뜯어보기 쉽도록

--- packages/core/src/db.ts
+++ packages/core/src/db.ts
@@ -1,10 +1,14 @@
-import type { Database } from "./tables.js";
 import SQLite from "better-sqlite3";
 import { Kysely, SqliteDialect } from "kysely";
+import type { Database } from "./tables.js";
 
-const dialect = new SqliteDialect({
-  database: new SQLite(":memory:"),
-});
+export const createDialect = (filename: string) => {
+  return new SqliteDialect({
+    database: new SQLite(filename),
+  });
+};
+
+const dialect = createDialect(":memory:");
 
 export const db = new Kysely<Database>({
   dialect,
--- packages/core/src/dev.ts
+++ packages/core/src/dev.ts
@@ -1,11 +1,14 @@
+import fs from "node:fs/promises";
 import { serve } from "@hono/node-server";
+import { Kysely } from "kysely";
 import type { Insertable } from "kysely";
 import { app } from "./app.js";
-import { db } from "./db.js";
-import { MasterData } from "./masterdata/instances.js";
+import { createDialect, db } from "./db.js";
 import { journalContext } from "./instances.js";
+import { MasterData } from "./masterdata/instances.js";
 import type {
   AccountCodeTable,
+  Database,
   JournalEntryLineTable,
   JournalEntryTable,
 } from "./tables.js";
@@ -26,7 +29,7 @@ for (const entry of journalContext.entries) {
 
 // in-memory DB 초기화
 // 데이터를 다양한 방식으로 뒤지려면 db에 채워놓는게 나을듯
-const insertBulk_accountCode = async () => {
+const insertBulk_accountCode = async (db: Kysely<Database>) => {
   await db.schema
     .createTable("accountCode")
     .addColumn("code", "integer", (c) => c.primaryKey())
@@ -55,7 +58,7 @@ const insertBulk_accountCode = async () => {
   await db.insertInto("accountCode").values(items).execute();
 };
 
-const insertBulk_journalEntry = async () => {
+const insertBulk_journalEntry = async (db: Kysely<Database>) => {
   await db.schema
     .createTable("journalEntry")
     .addColumn("txid", "varchar(255)", (c) => c.primaryKey())
@@ -75,7 +78,7 @@ const insertBulk_journalEntry = async () => {
   await db.insertInto("journalEntry").values(items).execute();
 };
 
-const insertBulk_journalEntryLine = async () => {
+const insertBulk_journalEntryLine = async (db: Kysely<Database>) => {
   await db.schema
     .createTable("journalEntryLine")
     .addColumn("txid", "varchar(255)")
@@ -110,12 +113,28 @@ const insertBulk_journalEntryLine = async () => {
   await db.insertInto("journalEntryLine").values(items).execute();
 };
 
-const initializeDatabase = async () => {
-  await insertBulk_accountCode();
-  await insertBulk_journalEntry();
-  await insertBulk_journalEntryLine();
+const initializeDatabase = async (db: Kysely<Database>) => {
+  await insertBulk_accountCode(db);
+  await insertBulk_journalEntry(db);
+  await insertBulk_journalEntryLine(db);
 };
-initializeDatabase();
+initializeDatabase(db);
+
+// TODO: 파일기반 sqlite에도 쓰면 내부를 볼수 있을듯?
+const writeDatabaseFile = async () => {
+  const fp = "db.sqlite";
+  try {
+    await fs.unlink(fp);
+  } catch (e) {
+    // ignore
+  }
+
+  const dialect = createDialect(fp);
+  const db = new Kysely<Database>({ dialect });
+  await initializeDatabase(db);
+  await db.destroy();
+};
+await writeDatabaseFile();
 
 serve(app, (info) => {
   console.log(`Listening on http://localhost:${info.port}`);
