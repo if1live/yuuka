49624763b413fb3fa37e331e1576a0d6bbde2cbf
if1live <libsora25@gmail.com>
2024-03-13 00:42:47 +0900

    분개장에서 경계선 대충 긋기
    트랜잭션 단위를 쉽게 보고 싶다

--- packages/frontend/src/features/journals/components/JournalEntryBlock.tsx
+++ packages/frontend/src/features/journals/components/JournalEntryBlock.tsx
@@ -20,6 +20,7 @@ export const JournalEntryBlock = (props: {
     // 올바른 journal entry는 적어도 2개의 행을 갖는다
     const isFirstRow = idx === 0;
     const isSecondRow = idx === 1;
+    const isLastRow = idx === entry.lines.length - 1;
 
     const debit = line._tag === "debit" ? line.debit : null;
     const credit = line._tag === "credit" ? line.credit : null;
@@ -30,11 +31,18 @@ export const JournalEntryBlock = (props: {
     const result = JournalEntry.safeValidate(entry);
     const error = result.isErr() ? (result.error as Error) : null;
 
+    // journal entry의 경계선을 긋고싶다
+    const cellClassName = isLastRow ? "last-row" : undefined;
+
+    // TODO: 더 멀쩡한 css 작성 방식이 필요
+    // 경계선이 어디까지 필요하지?
+    const style = isLastRow ? { borderBottom: "1px solid #ccc" } : undefined;
+
     return (
       <TableRow key={key} error={result.isErr()}>
         <TableCell>{displayDate ? entry.date : null}</TableCell>
 
-        <TableCell>
+        <TableCell className={cellClassName} style={style}>
           {isFirstRow ? (
             <>
               {entry.brief} | <JournalEntryLink id={entry.id} />
@@ -43,15 +51,15 @@ export const JournalEntryBlock = (props: {
           {isSecondRow && error ? error.message : null}
         </TableCell>
 
-        <TableCell>
+        <TableCell className={cellClassName} style={style}>
           <AccountCodeLink code={line.code} />
         </TableCell>
 
-        <TableCell textAlign="right">
+        <TableCell className={cellClassName} style={style} textAlign="right">
           {debit ? <CurrencyDisplay amount={debit} /> : null}
         </TableCell>
 
-        <TableCell textAlign="right">
+        <TableCell className={cellClassName} style={style} textAlign="right">
           {credit ? <CurrencyDisplay amount={credit} /> : null}
         </TableCell>
       </TableRow>
