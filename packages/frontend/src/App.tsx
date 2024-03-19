b3caeb14788fa8785ad3a59b35e49c639943104a
if1live <libsora25@gmail.com>
2024-03-19 23:56:49 +0900

    hash router

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import {
   Route,
   RouterProvider,
-  createBrowserRouter,
+  createHashRouter,
   createRoutesFromElements,
 } from "react-router-dom";
 import { QueryParamProvider } from "use-query-params";
@@ -14,7 +14,7 @@ import { JournalRouter } from "./routes/JournalRoute";
 import { LedgerRouter } from "./routes/LedgerRoute";
 import { Root } from "./routes/Root";
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
       <Route path="/journal/*" element={<JournalRouter />} />
@@ -22,7 +22,11 @@ const router = createBrowserRouter(
     </Route>,
   ),
   {
-    basename: "/yuuka",
+    // hash router에서는 없어야 제대로 작동한다.
+    // browser router 쓸때는 basename 붙어야 작동한다.
+    // github pages 기준으로는 어차피 정적 파일밖에 안되니까 hash router로 선택
+    // index.html을 404.html로 복사하는 편법을 쓰면 browser router 쓸 수 있긴하더라
+    // basename: "/yuuka",
   },
 );
 
