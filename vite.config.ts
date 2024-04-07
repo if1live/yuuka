da777ebc65b5af260e20634e874b263578d74401
if1live <libsora25@gmail.com>
2024-04-08 01:14:55 +0900

    rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 const baseUrl_ghpage = "/yuuka/";
 const baseUrl_vercel = undefined;
@@ -7,6 +8,6 @@ const baseUrl = process.env.VERCEL ? baseUrl_vercel : baseUrl_ghpage;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer()],
   base: baseUrl,
 });
