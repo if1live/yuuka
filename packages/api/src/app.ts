54928e9b651a61cefdd01f30e6bb45e83ab105a8
if1live <libsora25@gmail.com>
2024-06-23 19:09:10 +0900

    에러 핸들러 대충 붙임

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -7,6 +7,19 @@ import { HealthCheckApp, LedgerApi, ResourceApi } from "./routes/index.js";
 
 export const app = new Hono();
 
+app.onError((err, c) => {
+  console.error(err);
+
+  // 에러를 적당히 json으로 포장하면 편할듯
+  const data = {
+    name: err.name,
+    message: err.message,
+    cause: err.cause,
+    stack: err.stack?.split("\n").map((x) => x.trim()),
+  };
+  return c.json(data, 500);
+});
+
 app.use("*", cors());
 app.use(compress());
 app.use(prettyJSON());
--- packages/ui/src/fetchers.ts
+++ packages/ui/src/fetchers.ts
@@ -1,6 +1,15 @@
 import { supabase } from "./constants";
 
-const host = "http://127.0.0.1:3000";
+const host_localhost = "http://127.0.0.1:3000";
+
+// 귀찮아서 하드코딩. custom domain으로 나중에 교체
+const host_lambda =
+  "https://9hd4nt7gl2.execute-api.ap-northeast-1.amazonaws.com";
+
+const host_dev = host_localhost;
+const host_prod = host_lambda;
+
+const host = import.meta.env.DEV ? host_dev : host_prod;
 
 export const myfetch = async (input: string, init?: RequestInit) => {
   const result_session = await supabase.auth.getSession();
@@ -22,6 +31,29 @@ export const myfetch = async (input: string, init?: RequestInit) => {
     ...init,
     headers,
   });
-  const json = await resp.json();
-  return json;
+  if (resp.status < 400) {
+    const json = await resp.json();
+    return json;
+  }
+
+  // json으로 읽을수 있는 경우
+  let json: Record<string, string> | undefined = undefined;
+  try {
+    json = await resp.json();
+  } catch (e) {
+    json = undefined;
+  }
+
+  if (typeof json !== "undefined") {
+    const name = json.name;
+    const message = json.message;
+
+    const e = Error(message ?? "unknown error");
+    e.name = name;
+
+    throw e;
+  }
+
+  const text = await resp.text();
+  throw new Error(`fetch failed: ${resp.statusText}`);
 };
