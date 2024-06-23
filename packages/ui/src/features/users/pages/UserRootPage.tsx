196eecd6f699a18bba54a8e63686bc51ba882545
if1live <libsora25@gmail.com>
2024-06-23 19:09:04 +0900

    마스터데이터 대강 볼수 있도록

--- packages/ui/src/features/users/pages/UserRootPage.tsx
+++ packages/ui/src/features/users/pages/UserRootPage.tsx
@@ -1,12 +1,31 @@
 import { Container } from "@mantine/core";
 import { SupabaseSignOutButton } from "../components/SupabaseSignOutButton.js";
+import { useContext } from "react";
+import { MasterDataContext } from "../../../providers/MasterDataContext.js";
 
 export const UserRootPage = () => {
+  const masterdata = useContext(MasterDataContext);
+  const { accounts, presets } = masterdata;
+
   return (
     <Container>
       <h1>user root</h1>
 
       <SupabaseSignOutButton />
+
+      <h2>accounts</h2>
+      <ul>
+        {accounts.map((account) => (
+          <li key={account.name}>{account.name}</li>
+        ))}
+      </ul>
+
+      <h2>presets</h2>
+      <ul>
+        {presets.map((preset) => (
+          <li key={preset.name}>{preset.name}</li>
+        ))}
+      </ul>
     </Container>
   );
 };
