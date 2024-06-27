71864ae3e0cd13015950c4a51dde266ccbb2e429
if1live <libsora25@gmail.com>
2024-06-27 13:46:19 +0900

    pwa test

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,4 +1,5 @@
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
@@ -8,6 +9,32 @@ const baseUrl = process.env.VERCEL ? baseUrl_vercel : baseUrl_ghpage;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer()],
+  plugins: [
+    react(),
+    visualizer(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "yuuka · financial ledger",
+        short_name: "yuuka",
+        description: "financial ledger",
+        display: "standalone",
+        theme_color: "#6276BF",
+        background_color: "#6276BF",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   base: baseUrl,
 });
